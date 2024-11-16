ec40d6d9cc68c511e3f146d61dcd0ade41a3172b
SanSaahov <soyzov@gmail.com>
2024-11-16 10:26:39 +0300

    изменен title

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <HelloWorld msg="GitHub gh-pages/deploy_01"/>
 </template>
 
 <script>
